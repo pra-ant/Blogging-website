be8ff8c5d0b54d238a26db1d5dedbbcc5ece98c9
pra-ant <singhprabhakar879@gmail.com>
2024-11-14 19:09:37 +0530

    adding my work

---
+++ models/listing.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const review = require('./review.js');
+let listingSchema = new schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    image:{
+        url: String,
+        filename: String,
+    },
+    price:Number,
+    location:String,
+    country:String,
+    reviews:[
+        {
+            type:schema.Types.ObjectId,
+            ref:'review'
+        }
+    ],
+    owner:{
+        type:schema.Types.ObjectId,
+            required: true,
+        ref:'User',
+    },
+    geometry:{
+        type:{
+            type:String,
+            default: true,
+        },
+        coordinates:{
+            type:[Number],
+            required:true
+        }
+    },
+    category:{
+        type:String,
+        required:true
+    }
+});
+
+listingSchema.post("findOneAndDelete",async(list)=>{
+    if(list){
+        await review.deleteMany({_id :{$in: list.reviews}});
+    }
+    
+})
+
+const listing = mongoose.model("listing",listingSchema);
+
+module.exports = listing;
